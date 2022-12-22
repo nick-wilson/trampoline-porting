1e141d3ea6a0e52b86b538a729a2605b85e068b0
Nick Wilson <12475993+nick-wilson@users.noreply.github.com>
2022-12-22 09:13:40 +0000

    updated start.sh to ssh into container

--- docker/centos7-runtime/start.sh
+++ docker/centos7-runtime/start.sh
@@ -8,3 +8,10 @@ while [ $i -lt $ncontainer ] ; do
  docker run --rm -d -v $scratch:/scratch molpro-trampoline:centos7-runtime
  ((i=i+1))
 done
+
+# use "docker inspect network bridge" to identify IP addresses of containers
+ip1=172.23.0.2
+ip2=172.23.0.3
+
+# extract user ssh keys
+ssh -i id_rsa_docker user@$ip1
